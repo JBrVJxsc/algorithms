2786548ccb3f1f72eca1ab85e2a2a2db61fabd33
laonawuli <laonawuli@126.com>
2014-08-24 19:20:03 +0800

    Code

--- src/com/algorithms/algorithms/graphs/undirected/DFS.java
+++ src/com/algorithms/algorithms/graphs/undirected/DFS.java
@@ -1,5 +1,7 @@
 package com.algorithms.algorithms.graphs.undirected;
 
+import java.util.Stack;
+
 /**
  * Created by Who on 2014/8/22.
  */
@@ -23,5 +25,21 @@ public class DFS {
                 edgeTo[i] = v;
             }
         }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        for (; v != start; v = edgeTo[v]) {
+            stack.push(v);
+        }
+        stack.push(start);
+        return stack;
     }
 }
